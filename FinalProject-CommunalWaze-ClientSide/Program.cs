7b489ba578aa46d60b3ac0e4a79ee834cd52b76f
Devorah <devorah.wajs@gmail.com>
2024-04-08 12:23:46 +0300

    Add project Dal

--- FinalProject-CommunalWaze-ClientSide/Program.cs
+++ FinalProject-CommunalWaze-ClientSide/Program.cs
@@ -7,6 +7,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("https://localhost:7071", "http://localhost:3000")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
